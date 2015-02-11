1e74f64706e29bd33b4fb3e7d2460e0f959d7c7c
ssbabu95 <babu.sumeet1@gmail.com>
2015-02-11 16:40:50 -0500

    Drew string HEY BUDDY

--- MultipleChoiceQuestion.java
+++ MultipleChoiceQuestion.java
@@ -1,7 +1,13 @@
 // @ Martin Kellogg ; 2/11/15
 
 import javax.swing.JPanel;
+import java.awt.Graphics;
 
 public class MultipleChoiceQuestion extends JPanel {
 
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawString("HEY BUDDY", 10, 10);
+	}
 }
