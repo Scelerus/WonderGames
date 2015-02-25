f0c483a60afc24c9932af369277bfb520d8b5d50
ndc9jh <ndc9jh@virginia.edu>
2015-02-25 16:05:04 -0500

    Added ability to randomize answers and set up action listeners for the buttons

--- Driver.java
+++ Driver.java
@@ -1,13 +1,20 @@
-// @ Martin Kellogg ; 2/11/15
+// Owner: Martin Kellogg
+
+import java.util.Random;
 
 import javax.swing.JFrame;
 
 public class Driver {
+	final static long SEED = 0;
+	
     public static void main (String[] args){
 	JFrame j = new JFrame();
+	Random r = new Random(SEED);
+	
+	MCQ q = new MCQ("Question", "Right", "Wrong", "Wrong2", "Wrong3");
 	j.setSize(400,600);
 	j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	j.setContentPane(new MCQDisplay());
+	j.setContentPane(new MCQDisplay(q, r));
 	j.setVisible(true);
     }
 }
