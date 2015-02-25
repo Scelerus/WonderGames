c627779c186501bf7a74b0cc1c662fd3ad83b1e7
sabinayim <sjy8hy@virginia.edu>
2015-02-25 15:58:05 -0500

    MCQInput
    Added MCQInput method for MCQ class - gets String filename input and
    use scanner to go through it, get question and answers, make an MCQ out
    of it and return it

--- Driver.java
+++ Driver.java
@@ -3,11 +3,12 @@
 import javax.swing.JFrame;
 
 public class Driver {
-    public static void main (String[] args){
-	JFrame j = new JFrame();
-	j.setSize(400,600);
-	j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	j.setContentPane(new MCQDisplay());
-	j.setVisible(true);
-    }
+	public static void main(String[] args) {
+		JFrame j = new JFrame();
+		j.setSize(400, 600);
+		j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		MCQ mcq = new MCQ("2+2","4","5","0","6");
+		j.setContentPane(new MCQDisplay(mcq));
+		j.setVisible(true);
+	}
 }
