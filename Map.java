29e4a60dbf477ba197bf994ec12a17ff322c06ee
ndc9jh <ndc9jh@virginia.edu>
2015-03-04 16:37:59 -0500

    Changed Tile class to Til and made Array fixed size

--- Map.java
+++ Map.java
@@ -15,7 +15,7 @@ public class Map extends JPanel implements KeyListener{
     final int TILE_HEIGHT = 200;
 
     Random r;
-    Tile[][] tiles;
+    Til[][] tiles;
     public Map (ArrayList<MCQ> rgmcq, Random r){
 	this.r = r;
 	KeyListener listener = new KeyListener();
@@ -26,10 +26,10 @@ public class Map extends JPanel implements KeyListener{
 	//create tiles here. Use mcq_s from the array list that's passed in
 	int rgmcqLength = (int) Math.sqrt(rgmcq.size());
 	int rgmcqWidth = rgmcq.size() - rgmcqLength;
-	this.tiles = new Tile[rgmcqLength][rgmcqWidth];
-		for (int j = 0; j < rgmcqLength; j++) {
-			for (int k = 0; k < rgmcqWidth; k++) {
-				tiles[j][k] = new Tile(rgmcq.get(0), "Image");
+	this.tiles = new Til[10][10];
+		for (int j = 0; j < 10; j++) {
+			for (int k = 0; k < 10; k++) {
+				tiles[j][k] = new Til(rgmcq.get(r.nextInt(rgmcq.size())), "Image");
 			}
 			
 		}
