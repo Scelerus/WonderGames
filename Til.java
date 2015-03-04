a92b157103f892fc3cc9c4f0f906b5b1d353abe0
ndc9jh <ndc9jh@virginia.edu>
2015-03-04 16:38:45 -0500

    Added Til class (I forgot it)

---
+++ Til.java
@@ -0,0 +1,16 @@
+
+public class Til {
+
+	private MCQ q;
+	private String fileName;
+
+	public Til(MCQ question, String imageFile) {
+		this.q = question;
+		this.fileName = imageFile;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+}
