2175e4f204582e3e88e4dfbb0ff714eeacbd23c3
Sumeet Babu <sumeetbabu@d-172-27-109-17.bootp.Virginia.EDU>
2015-03-04 16:09:10 -0500

    Added key listener

--- Map.java
+++ Map.java
@@ -1,7 +1,8 @@
 import javax.swing.JPanel;
 import java.util.ArrayList;
 import java.util.Random;
-
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
 
 public class Map extends JPanel {
 
@@ -9,7 +10,18 @@ public class Map extends JPanel {
 
     public Map (ArrayList<MCQ> rgmcq, Random r){
 	this.r = r;
+	KeyListener listener = new MyKeyListener();
+	addKeyListener(listener);
+	setFocusable(true);
 	//create tiles here. Use mcq_s from the array list that's passed in
+    }
+
+    public class MyKeyListener implements KeyListener {
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+	}
+
     }
 
 }
