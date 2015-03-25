6e5c8fd3376ea69c94125a9cc9d7dd1dd6a64c45
Scelerus <scelerus@scelerus-1015E.(none)>
2015-03-25 16:00:18 -0400

    fixed compile errors

--- Opp.java
+++ Opp.java
@@ -1,10 +1,14 @@
 
+import javax.swing.JFrame;
+import java.util.Random;
+
+
 public class Opp extends Character {
    
     MCQ mcq = null;
     Random r = null;
 
-    public Opp (Mcq mcq, Random r, int x, int y){
+    public Opp (MCQ mcq, Random r, int x, int y){
 	this.mcq = mcq;
 	this.r = r;
 	this.setxCoord(x);
@@ -14,7 +18,7 @@ public class Opp extends Character {
 	JFrame j = new JFrame();
 	j.setSize(400,600);
 	j.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-	j.setContentPane(new MCQDisplay(q, r, j));
+	j.setContentPane(new MCQDisplay(mcq, r, j));
 	j.setVisible(true);
 	return j;
     }
